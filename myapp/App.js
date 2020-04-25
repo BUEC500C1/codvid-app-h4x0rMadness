f35d0e694691fa5206906b2b4821cd8ff0accc24
Kefan Zhang <939149050@qq.com>
2020-04-25 04:13:36 -0400

    update api version

--- myapp/App.js
+++ myapp/App.js
@@ -5,9 +5,99 @@ import MapCircle from "react-native-maps/lib/components/MapCircle";
 import MapMarker from "react-native-maps/lib/components/MapMarker";
 import data from "./countries";
 
-class Blink extends Component {
-    // 声明state对象
-    state = { isShowingText: true };
+class Covid_Info_Init extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            country_name_json: data,
+            TotalConfirmed: '',
+            TotalDeaths: '',
+            StartDate: '2000-01-01T00:00:00.000Z',
+            EndDate: '',
+            summary_json: '',
+            CountriesJson: '',
+            localConfirmed: '',
+            localDeaths: '',
+            localRecovered: '',
+            globalConfirmed: '',
+            globalDeaths: '',
+            globalRecovered: '',
+            random: '',
+        }
+    }
+    componentDidMount() {
+        this.renderMarkers();
+        let newDate = new Date();
+        this.state.EndDate = newDate.toISOString();
+        fetch('https://api.covid19api.com/summary')
+            .then((response) => response.json())
+            .then(responseJson => {
+                this.setState({
+                        summary_json: responseJson,
+                        globalConfirmed: responseJson['Global']['TotalConfirmed'],
+                        globalDeath: responseJson['Global']['TotalDeaths'],
+                        globalRecovered: responseJson['Global']['TotalRecovered'],
+                    },
+                    function(){}
+                );
+                console.log(this.state.globalConfirmed+','+this.state.globalDeath+','+this.state.globalRecovered);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+    //https://api.covid19api.com/country/south-africa?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z
+    //https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z
+    //https://api.covid19api.com/country/south-africa
+    get_info(name) {
+        //const link = "https://api.covid19api.com/country/" + name;
+        var c = 0, d = 0, r = 0;
+        var a = fetch("https://api.covid19api.com/country/" + name)
+            .then((response) => response.json())
+            .then(responseJson => {
+                if (responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]) {
+                    return [responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Confirmed'],
+                    responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Deaths'],
+                    responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Recovered']];
+                }
+            });
+        console.log(typeof(a));
+        return [c,d,r];
+    }
+    renderMarkers() {
+        return data.map((country,i) =>{
+            const a = this.get_info(country.CountryName);
+            
+            return (
+                <Marker
+                    key = {i}
+                    coordinate={{latitude: country.CapitalLatitude, longitude: country.CapitalLongitude}}
+                    title={country.CountryName}
+                    description={"Confirmed: " + a[0] + " Deaths: "  +
+                                " Recovered: " }
+                    pinColor={'#99001d'}
+                />
+            )
+        })
+        
+    }
+    
+    
+    render() {
+        return this.renderMarkers();
+    }
+}
+
+class Display_Markers extends Component
+{
+    constructor(props) {
+        super(props);
+        this.state = {
+            Confirmed:  '',
+            Deaths:     '',
+            Recovered:  '',
+        }
+    }
 
     componentDidMount() {
         // 每1000毫秒对showText状态做一次取反操作
@@ -17,138 +107,65 @@ class Blink extends Component {
             });
         }, 1000);
     }
-
-    render() {
-        // 根据当前showText的值决定是否显示text内容
-        if (!this.state.isShowingText) {
-            return null;
-        }
-
-        return (
-            <View style={{position: 'absolute', top: 800, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
-                <Text>Centered text</Text>
-            </View>
-            //<Text>{this.props.text}</Text>
-        );
+    get_info(name) {
+        fetch("https://api.covid19api.com/country/" + name)
+            .then((response) => response.json())
+            .then(responseJson => {
+                if (responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]) {
+                    this.state.Confirmed = responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Confirmed'].toString();
+                    this.state.Deaths = responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Deaths'].toString();
+                    this.state.Recovered = responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Recovered'].toString();
+                }
+            });
+        
+        
     }
-}
-class Covid_Info_Init extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            json_data: data,
-            TotalConfirmed: '',
-            TotalDeaths: '',
-            Date: '',
-        }
-    }
-    componentDidMount() {
-
-    }
-    renderMarkers() {
-        return this.state.json_data.map((country,i) =>{
+    
+    renderMarkers()
+    {
+        return data.map((country, i) => {
             return (
                 <Marker
-                    key = {i}
+                    key={i}
                     coordinate={{latitude: country.CapitalLatitude, longitude: country.CapitalLongitude}}
                     title={country.CountryName}
-                    description="Current Cases: "
+                    description={"Confirmed: " + this.state.Confirmed + " " +
+                                 "Deaths: "    + this.state.Deaths + " " +
+                                "Recovered: " + this.state.Recovered}
                     pinColor={'#99001d'}
+                    onPress={e => {
+                        console.log(country.CountryName + " is pressed");
+                        console.log(this.state.Confirmed);
+                        this.get_info(country.CountryName);
+                        
+                    }}
                 />
             )
         })
+
     }
     render() {
-        return this.renderMarkers()
-    }
-
-}
-
-class Get_data extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            TotalConfirmed: '',
-            TotalDeaths: '',
-            Date: '',
-        };
-    }
-
-    componentDidMount() {
-        fetch('https://api.covid19api.com/summary')
-            .then((response) => response.json())
-            .then(responseJson => {
-                this.setState({
-                        TotalConfirmed: responseJson['Countries'][235]['TotalConfirmed'],
-                        TotalDeaths: responseJson['Countries'][235]['TotalDeaths'],
-                        Date: responseJson['Countries'][235]['Date'],
-                    },
-                    function(){}
-                );
-                console.log(this.state.jsondata);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }
-    //<Text style = {style.text}>Total Confirmed Cases: {this.state.TotalConfirmed}</Text>
-    render(){
-
-        return(
-
-            <Marker
-                coordinate={{latitude: 38, longitude: -122}}
-                title={this.state.TotalConfirmed.toString() + "my dude!"}
-                description="this is a marker example"
-                pinColor={'#990300'}
-            />
-        )
+        return this.renderMarkers();
     }
 }
-//class NameForm extends React.Component {
-//    constructor(props) {
-//        super(props);
-//        this.state = {value: ''};
-//
-//        this.handleChange = this.handleChange.bind(this);
-//        this.handleSubmit = this.handleSubmit.bind(this);
-//    }
-//
-//    handleChange(event) {
-//        this.setState({value: event.target.value});
-//    }
-//
-//    handleSubmit(event) {
-//        alert('提交的名字: ' + this.state.value);
-//        event.preventDefault();
-//    }
-//
-//    render() {
-//        return (
-//            <form onSubmit={this.handleSubmit}>
-//                <input type=”text” name=”email” />
-//            </form>
-//        );
-//    }
-//}
 export default class App extends Component {
-
+    
     render() {
-
+        
         return (
-
             <View style={{position: 'relative', height: "100%"}}>
 
-                
+
                 <MapView
                     style={styles.map}
                     mapType={"hybrid"}
                     maxZoomLevel={3}>
-                    <Covid_Info_Init/>
+                    <Display_Markers/>
 
                 </MapView>
-
+                
             </View>
+            
         );
     }
 }
--- myapp/backup.js
+++ myapp/backup.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {Text, View} from "react-native";
+import {Marker} from "react-native-maps";
 
 class Blink extends Component {
     // 声明state对象
@@ -34,3 +35,45 @@ class Blink extends Component {
 <Blink text='Yes blinking is so great' />
 <Blink text='Why did they ever take this out of HTML' />
 <Blink text='Look at me look at me look at me' />
+    
+    class Get_data extends Component {
+        constructor(props){
+            super(props);
+            this.state = {
+                TotalConfirmed: '',
+                TotalDeaths: '',
+                Date: '',
+            };
+        }
+
+        componentDidMount() {
+            fetch('https://api.covid19api.com/summary')
+                .then((response) => response.json())
+                .then(responseJson => {
+                    this.setState({
+                            TotalConfirmed: responseJson['Countries'][235]['TotalConfirmed'],
+                            TotalDeaths: responseJson['Countries'][235]['TotalDeaths'],
+                            Date: responseJson['Countries'][235]['Date'],
+                        },
+                        function(){}
+                    );
+
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+        //<Text style = {style.text}>Total Confirmed Cases: {this.state.TotalConfirmed}</Text>
+        render(){
+
+            return(
+
+                <Marker
+                    coordinate={{latitude: 38, longitude: -122}}
+                    title={this.state.TotalConfirmed.toString() + "my dude!"}
+                    description="this is a marker example"
+                    pinColor={'#990300'}
+                />
+            )
+        }
+    }
