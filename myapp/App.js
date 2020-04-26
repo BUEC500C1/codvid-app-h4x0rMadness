2617ba2b30cc27b7cfa10e6ca47daf2c5873f371
Kefan Zhang <939149050@qq.com>
2020-04-26 04:28:14 -0400

    final version

--- myapp/App.js
+++ myapp/App.js
@@ -25,26 +25,40 @@ class Display_Markers extends Component
             Confirmed:  '',
             Deaths:     '',
             Recovered:  '',
+            lat: '',
+            lon: '',
         }
     }
+    reduce_markers() {
+        var subarr = [];
+        for(var i = 0; i < this.state.markers.length && i < 420; i++){
+            subarr[i] = this.state.markers[i];
+        }
+        this.setState({
+            markers: subarr,
+        });
+    }
+
     get_info(name) {
         //https://api.covid19api.com/total/country/china
         console.log("parse in info: " + name);
         fetch("https://api.covid19api.com/total/country/" + name)
             .then((response) => response.json())
             .then(responseJson => {
+                console.log(responseJson);
+                
                 if (responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]) {
                     this.setState({
-                        Confirmed: responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Confirmed'].toString(),
-                        Deaths: responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Deaths'].toString(),
-                        Recovered: responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Recovered'].toString(),
+                        Confirmed: responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Confirmed'],
+                        Deaths: responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Deaths'],
+                        Recovered: responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Recovered'],
                     });
                 }
             });
     }
     
     fetch_markers() {
-        //if(this.state.markers) this.state.markers.pop();
+        //this.state.markers = [];
         for(var i = 0; i < Object.keys(data).length - 1; i++){
             var item = data[i];
             var marker = <Marker
@@ -55,30 +69,39 @@ class Display_Markers extends Component
                 onPress={this._onMarkerPress.bind(this)}
             />;
             this.state.markers.push(marker);
+            
         }
+        console.log("len of array: "+this.state.markers.length)
             
     }
     
     get_country(lat, lon) {
         //https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=YOUR_API_KEY
-        fetch("https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&result_type=country&key=YOURAPIKEY")
+        //https://maps.googleapis.com/maps/api/geocode/json?latlng=45,-75&key=AIzaSyD3KeUMIqlikl0HLvhqojXGOZ6e6EfLDdg
+        //"https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&result_type=country&key=AIzaSyD3KeUMIqlikl0HLvhqojXGOZ6e6EfLDdg"
+        fetch("https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&result_type=country&key=AIzaSyD3KeUMIqlikl0HLvhqojXGOZ6e6EfLDdg"
+        )
             .then((response) => response.json())
             .then(responseJson => {
-                console.log(responseJson);
-                console.log(responseJson['results']['formatted_address']);
-                if(responseJson)
-                    this.setState({
-                        countryname: responseJson['results']['formatted_address']
-                    })
+                //console.log("lethal legal name: " +JSON.stringify(responseJson.results["0"]["formatted_address"]))
+                this.setState({
+                    countryname: JSON.stringify(responseJson.results["0"]["formatted_address"])
+                });
+                    
             });
-        console.log(this.state.countryname);
+        console.log("lmc"+this.state.countryname);
     }
     _onMarkerPress (mkr) {
         //console.log("if worked..."+mkr.Marker.title);
+        //this.reduce_markers();
         var res = mkr.nativeEvent.coordinate;
-        console.log("lat" +res['latitude']+"lon"+res['longitude']);
+        console.log("lat " +res['latitude']+" lon "+res['longitude']);
+        
         this.get_country(res['latitude'],res['longitude']);
-        console.log(this.state.countryname);
+        console.log("countryname: "+this.state.countryname);
+        var substr = this.state.countryname.substring(1, this.state.countryname.length - 1);
+        this.state.countryname= substr;
+        
         if(this.state.countryname) 
             this.get_info(this.state.countryname);
         this.state.markers.push(
@@ -89,6 +112,7 @@ class Display_Markers extends Component
                 name={this.state.countryname}
             />
         );
+        console.log("type: " + typeof(this.state.countryname))
         this.forceUpdate();
         
     }
@@ -114,10 +138,11 @@ class Display_Data extends Component {
         //console.log("c" + this.props.c)
     }
     render() {
+        console.log("c in dd: " + this.state.confirmed);
         return (
             <View
                 style={{position: 'absolute', top: 700, left: 0, right: 0, bottom: 0, justifyContent: 'center',
-                    alignItems: 'center' }}>
+                    alignItems: 'flex-start' }}>
                 <Text style={{fontSize: 20, color: 'white'}}>
                     Country:  {this.state.country}
                 </Text>
