6f2723c01d5179937ef82aadd9bf18bfdd99ddba
Kefan Zhang <939149050@qq.com>
2020-04-26 02:35:35 -0400

    update

--- myapp/App.js
+++ myapp/App.js
@@ -4,148 +4,134 @@ import MapView, {Circle, Marker} from 'react-native-maps';
 import MapCircle from "react-native-maps/lib/components/MapCircle";
 import MapMarker from "react-native-maps/lib/components/MapMarker";
 import data from "./countries";
-
-class Covid_Info_Init extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            country_name_json: data,
-            TotalConfirmed: '',
-            TotalDeaths: '',
-            StartDate: '2000-01-01T00:00:00.000Z',
-            EndDate: '',
-            summary_json: '',
-            CountriesJson: '',
-            localConfirmed: '',
-            localDeaths: '',
-            localRecovered: '',
-            globalConfirmed: '',
-            globalDeaths: '',
-            globalRecovered: '',
-            random: '',
-        }
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+    },
+    map: {
+        width: "100%",
+        height: "100%",
+        zIndex: -1
     }
-    componentDidMount() {
-        this.renderMarkers();
-        let newDate = new Date();
-        this.state.EndDate = newDate.toISOString();
-        fetch('https://api.covid19api.com/summary')
-            .then((response) => response.json())
-            .then(responseJson => {
-                this.setState({
-                        summary_json: responseJson,
-                        globalConfirmed: responseJson['Global']['TotalConfirmed'],
-                        globalDeath: responseJson['Global']['TotalDeaths'],
-                        globalRecovered: responseJson['Global']['TotalRecovered'],
-                    },
-                    function(){}
-                );
-                console.log(this.state.globalConfirmed+','+this.state.globalDeath+','+this.state.globalRecovered);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }
-    //https://api.covid19api.com/country/south-africa?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z
-    //https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z
-    //https://api.covid19api.com/country/south-africa
-    get_info(name) {
-        //const link = "https://api.covid19api.com/country/" + name;
-        var c = 0, d = 0, r = 0;
-        var a = fetch("https://api.covid19api.com/country/" + name)
-            .then((response) => response.json())
-            .then(responseJson => {
-                if (responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]) {
-                    return [responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Confirmed'],
-                    responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Deaths'],
-                    responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Recovered']];
-                }
-            });
-        console.log(typeof(a));
-        return [c,d,r];
-    }
-    renderMarkers() {
-        return data.map((country,i) =>{
-            const a = this.get_info(country.CountryName);
-            
-            return (
-                <Marker
-                    key = {i}
-                    coordinate={{latitude: country.CapitalLatitude, longitude: country.CapitalLongitude}}
-                    title={country.CountryName}
-                    description={"Confirmed: " + a[0] + " Deaths: "  +
-                                " Recovered: " }
-                    pinColor={'#99001d'}
-                />
-            )
-        })
-        
-    }
-    
-    
-    render() {
-        return this.renderMarkers();
-    }
-}
+});
 
 class Display_Markers extends Component
 {
     constructor(props) {
         super(props);
         this.state = {
+            countryname: '',
+            markers: [],
             Confirmed:  '',
             Deaths:     '',
             Recovered:  '',
         }
     }
-
-    componentDidMount() {
-        // 每1000毫秒对showText状态做一次取反操作
-        setInterval(() => {
-            this.setState({
-                isShowingText: !this.state.isShowingText
-            });
-        }, 1000);
-    }
     get_info(name) {
-        fetch("https://api.covid19api.com/country/" + name)
+        //https://api.covid19api.com/total/country/china
+        console.log("parse in info: " + name);
+        fetch("https://api.covid19api.com/total/country/" + name)
             .then((response) => response.json())
             .then(responseJson => {
                 if (responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]) {
-                    this.state.Confirmed = responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Confirmed'].toString();
-                    this.state.Deaths = responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Deaths'].toString();
-                    this.state.Recovered = responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Recovered'].toString();
+                    this.setState({
+                        Confirmed: responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Confirmed'].toString(),
+                        Deaths: responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Deaths'].toString(),
+                        Recovered: responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Recovered'].toString(),
+                    });
                 }
             });
-        
-        
     }
     
-    renderMarkers()
-    {
-        return data.map((country, i) => {
-            return (
-                <Marker
-                    key={i}
-                    coordinate={{latitude: country.CapitalLatitude, longitude: country.CapitalLongitude}}
-                    title={country.CountryName}
-                    description={"Confirmed: " + this.state.Confirmed + " " +
-                                 "Deaths: "    + this.state.Deaths + " " +
-                                "Recovered: " + this.state.Recovered}
-                    pinColor={'#99001d'}
-                    onPress={e => {
-                        console.log(country.CountryName + " is pressed");
-                        console.log(this.state.Confirmed);
-                        this.get_info(country.CountryName);
-                        
-                    }}
-                />
-            )
-        })
-
+    fetch_markers() {
+        //if(this.state.markers) this.state.markers.pop();
+        for(var i = 0; i < Object.keys(data).length - 1; i++){
+            var item = data[i];
+            var marker = <Marker
+                key = {i}
+                coordinate={{latitude: item.CapitalLatitude, longitude: item.CapitalLongitude}}
+                title={item.CountryName}
+                pinColor={'#99001d'}
+                onPress={this._onMarkerPress.bind(this)}
+            />;
+            this.state.markers.push(marker);
+        }
+            
+    }
+    
+    get_country(lat, lon) {
+        //https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=YOUR_API_KEY
+        fetch("https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&result_type=country&key=YOURAPIKEY")
+            .then((response) => response.json())
+            .then(responseJson => {
+                console.log(responseJson);
+                console.log(responseJson['results']['formatted_address']);
+                if(responseJson)
+                    this.setState({
+                        countryname: responseJson['results']['formatted_address']
+                    })
+            });
+        console.log(this.state.countryname);
+    }
+    _onMarkerPress (mkr) {
+        //console.log("if worked..."+mkr.Marker.title);
+        var res = mkr.nativeEvent.coordinate;
+        console.log("lat" +res['latitude']+"lon"+res['longitude']);
+        this.get_country(res['latitude'],res['longitude']);
+        console.log(this.state.countryname);
+        if(this.state.countryname) 
+            this.get_info(this.state.countryname);
+        this.state.markers.push(
+            <Display_Data
+                c={this.state.Confirmed} 
+                d={this.state.Deaths} 
+                r={this.state.Recovered} 
+                name={this.state.countryname}
+            />
+        );
+        this.forceUpdate();
+        
     }
     render() {
-        return this.renderMarkers();
+        this.fetch_markers();
+        return (
+            this.state.markers
+            
+        )
+        
+        
+    }
+}
+class Display_Data extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            country:    this.props.name,
+            confirmed:  this.props.c,
+            deaths:     this.props.d,
+            recovered:  this.props.r,
+        }    ;
+        //console.log("c" + this.props.c)
+    }
+    render() {
+        return (
+            <View
+                style={{position: 'absolute', top: 700, left: 0, right: 0, bottom: 0, justifyContent: 'center',
+                    alignItems: 'center' }}>
+                <Text style={{fontSize: 20, color: 'white'}}>
+                    Country:  {this.state.country}
+                </Text>
+                <Text style={{fontSize: 20, color: 'white'}}>
+                    Confirmed:  {this.state.confirmed}
+                </Text>
+                <Text style={{fontSize: 20, color: 'white'}}>
+                    Deaths:  {this.state.deaths}
+                </Text>
+                <Text style={{fontSize: 20, color: 'white'}}>
+                    Recovered:  {this.state.recovered}
+                </Text>
+            </View>
+        );
     }
 }
 export default class App extends Component {
@@ -161,22 +147,13 @@ export default class App extends Component {
                     mapType={"hybrid"}
                     maxZoomLevel={3}>
                     <Display_Markers/>
-
+                    
                 </MapView>
+                
                 
             </View>
             
         );
     }
 }
-const styles = StyleSheet.create({
-    container: {
-        ...StyleSheet.absoluteFillObject,
-    },
-    map: {
-        width: "100%",
-        height: "100%",
-        zIndex: -1
-    }
-});
 
--- myapp/backup.js
+++ myapp/backup.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {Text, View} from "react-native";
 import {Marker} from "react-native-maps";
+import data from "./countries";
 
 class Blink extends Component {
     // 声明state对象
@@ -77,3 +78,116 @@ class Blink extends Component {
             )
         }
     }
+
+//renderMarkers()
+//{
+//    return data.map((country, i) => {
+//        return (
+//            <Marker
+//                key={i}
+//                coordinate={{latitude: country.CapitalLatitude, longitude: country.CapitalLongitude}}
+//                title={country.CountryName}
+//                description= ''
+//                pinColor={'#99001d'}
+//                
+//                onPress={
+//                    
+//                    //this.__onMarkerPress.bind()
+//                   
+//                        //this.get_info(country.CountryName);
+//                        //console.log(country.CountryName + " "+"Confirmed: " + this.state.Confirmed + " " +"Deaths: "
+//                        //    + this.state.Deaths + " " +"Recovered: " + this.state.Recovered);
+//                        //this.get_info(country.CountryName);
+//                        //this.forceUpdate();
+//                        //this.state.isClicked =  true;
+//                    
+//                >/}
+//            />
+//            //<Display_Data c={this.state.Confirmed} d={this.state.Deaths} r={this.state.Recovered}/>]
+//        )
+//    })
+//
+//}
+
+class Covid_Info_Init extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            country_name_json: data,
+            TotalConfirmed: '',
+            TotalDeaths: '',
+            StartDate: '2000-01-01T00:00:00.000Z',
+            EndDate: '',
+            summary_json: '',
+            CountriesJson: '',
+            localConfirmed: '',
+            localDeaths: '',
+            localRecovered: '',
+            globalConfirmed: '',
+            globalDeaths: '',
+            globalRecovered: '',
+            random: '',
+        }
+    }
+    componentDidMount() {
+        this.renderMarkers();
+        let newDate = new Date();
+        this.state.EndDate = newDate.toISOString();
+        fetch('https://api.covid19api.com/summary')
+            .then((response) => response.json())
+            .then(responseJson => {
+                this.setState({
+                        summary_json: responseJson,
+                        globalConfirmed: responseJson['Global']['TotalConfirmed'],
+                        globalDeath: responseJson['Global']['TotalDeaths'],
+                        globalRecovered: responseJson['Global']['TotalRecovered'],
+                    },
+                    function(){}
+                );
+                console.log(this.state.globalConfirmed+','+this.state.globalDeath+','+this.state.globalRecovered);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+    //https://api.covid19api.com/country/south-africa?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z
+    //https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z
+    //https://api.covid19api.com/country/south-africa
+    get_info(name) {
+        //const link = "https://api.covid19api.com/country/" + name;
+        var c = 0, d = 0, r = 0;
+        var a = fetch("https://api.covid19api.com/country/" + name)
+            .then((response) => response.json())
+            .then(responseJson => {
+                if (responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]) {
+                    return [responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Confirmed'],
+                        responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Deaths'],
+                        responseJson[Object.keys(responseJson)[Object.keys(responseJson).length - 1]]['Recovered']];
+                }
+            });
+        console.log(typeof(a));
+        return [c,d,r];
+    }
+    renderMarkers() {
+        return data.map((country,i) =>{
+            const a = this.get_info(country.CountryName);
+
+            return (
+                <Marker
+                    key = {i}
+                    coordinate={{latitude: country.CapitalLatitude, longitude: country.CapitalLongitude}}
+                    title={country.CountryName}
+                    description={"Confirmed: " + a[0] + " Deaths: "  +
+                    " Recovered: " }
+                    pinColor={'#99001d'}
+                />
+            )
+        })
+
+    }
+
+
+    render() {
+        return this.renderMarkers();
+    }
+}
